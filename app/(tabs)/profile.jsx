cb58b34261ee264485c746a605dbdea0f4f1dc7a
Krishab Bashyal <krishab2002@gmail.com>
2024-05-02 15:59:34 -0500

    added signout functionality to profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import { auth } from "../../config/firebaseConfig";
+import { signOut } from "firebase/auth";
+import CustomButton from "../../components/CustomButton"
+import { router } from "expo-router";
+
 
 const Profile = () => {
   const [userEmail, setUserEmail] = useState('');
+
+  const handleUserSignOut = async () => {
+    await signOut(auth)
+    router.replace("/logIn")
+
+  }
   
   useEffect(() => {
     const user = auth.currentUser;
@@ -16,8 +26,9 @@ const Profile = () => {
 
   return (
     <SafeAreaView>
-      <View className="justify-center items-center h-full">
+      <View className="justify-center items-center h-full mx-8">
         <Text>{userEmail}</Text>
+        <CustomButton title="Sign Out" buttonStyles="w-full h-10 bg-EE-Red mt-4" textStyles="text-white" handlePress={handleUserSignOut}/>
       </View>
     </SafeAreaView>
   );
