7dcad9dfdc123a99cd5591fbead84ff8a3e769d0
Krishab Bashyal <krishab2002@gmail.com>
2024-05-03 20:07:04 -0500

    Started working on the search functionality and quick city filters

---
+++ components/ExploreQuickFilterScroller.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ExploreQuickFilterScroller = () => {
+  return (
+    <View className="flex flex-row gap-4">
+      <Text>Dallas, TX</Text>
+      <Text>Austin, TX</Text>
+      <Text>Texarkan, TX</Text>
+      <Text>Houston, TX</Text>
+      <Text>Little Rock, AR</Text>
+      <Text>Hot Springs, AR</Text>
+    </View>
+  )
+}
+
+export default ExploreQuickFilterScroller
