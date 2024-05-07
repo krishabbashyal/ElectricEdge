38dec24eef66ba8ed2a9efb4b1e6fb019b309f95
Krishab Bashyal <krishab2002@gmail.com>
2024-05-07 16:24:51 -0500

    Moved page padding from component to page

--- app/(tabs)/explore.jsx
+++ app/(tabs)/explore.jsx
@@ -1,32 +1,46 @@
 import { ScrollView, Text, View, SafeAreaView } from "react-native";
 import ExploreSearchAndFilter from "../../components/ExploreSearchAndFilter";
 import ExploreChargerCard from "../../components/ExploreChargerCard";
-import ExploreQuickFilterScroller from "../../components/ExploreQuickFilterScroller";
+import QuickCityFilterScroller from "../../components/QuickCityFilterScroller";
 import React from "react";
 
 const explore = () => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor:"white"}}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <ExploreSearchAndFilter />
-      <ExploreQuickFilterScroller/>
-      <ScrollView className="flex-1">
-        <View className="flex-col">
-          <ExploreChargerCard imageFile={require("../../assets/images/dallas.jpeg")} chargerType="Level 2: 240-Volt Outlet" chargerLocation="Dallas, TX" chargerRate="2.37" />
-          <ExploreChargerCard imageFile={require("../../assets/images/boston.jpeg")} chargerType="Level 2: 240-Volt Outlet" chargerLocation="Boston, MA" chargerRate="2.37" />
-          <ExploreChargerCard
-            imageFile={require("../../assets/images/losAngeles.jpeg")}
-            chargerType="Level 2: 240-Volt Outlet"
-            chargerLocation="Los Angeles, CA"
-            chargerRate="2.37"
-          />
-          <ExploreChargerCard
-            imageFile={require("../../assets/images/newYork.jpeg")}
-            chargerType="Level 2: 240-Volt Outlet"
-            chargerLocation="New York City, NY"
-            chargerRate="2.37"
-          />
+
+        <QuickCityFilterScroller />
+        <View className="mx-6 h-full mt-2">
+          <ScrollView className="flex-1">
+            <View className="flex-col">
+              <ExploreChargerCard
+                imageFile={require("../../assets/images/dallas.jpeg")}
+                chargerType="Level 2: 240-Volt Outlet"
+                chargerLocation="Dallas, TX"
+                chargerRate="2.37"
+              />
+              <ExploreChargerCard
+                imageFile={require("../../assets/images/boston.jpeg")}
+                chargerType="Level 2: 240-Volt Outlet"
+                chargerLocation="Boston, MA"
+                chargerRate="2.37"
+              />
+              <ExploreChargerCard
+                imageFile={require("../../assets/images/losAngeles.jpeg")}
+                chargerType="Level 2: 240-Volt Outlet"
+                chargerLocation="Los Angeles, CA"
+                chargerRate="2.37"
+              />
+              <ExploreChargerCard
+                imageFile={require("../../assets/images/newYork.jpeg")}
+                chargerType="Level 2: 240-Volt Outlet"
+                chargerLocation="New York City, NY"
+                chargerRate="2.37"
+              />
+            </View>
+          </ScrollView>
         </View>
-      </ScrollView>
+
     </SafeAreaView>
   );
 };
