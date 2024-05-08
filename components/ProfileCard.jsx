3b45d8cf7ede670fd37571f16553e1802c32ff0a
Krishab Bashyal <krishab2002@gmail.com>
2024-05-07 21:40:46 -0500

    Created profile card component

---
+++ components/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ProfileCard = () => {
+  return (
+    <View>
+      <Text>ProfileCard</Text>
+    </View>
+  )
+}
+
+export default ProfileCard
