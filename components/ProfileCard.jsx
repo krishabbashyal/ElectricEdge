b7f4069452676f968d49e8593aa1e2414658687c
Krishab Bashyal <krishab2002@gmail.com>
2024-05-08 22:50:41 -0500

    Removed extra safe area view

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -16,21 +16,18 @@ const ProfileCard = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      
-        <View className="flex-row items-center pb-4 justify-between mt-9 border-b border-gray-300 h-20">
-          <View className="flex-row items-center">
-            <Image className="w-14 h-14 rounded-full" source={require(".././assets/images/profilePicture.png")} />
-            <View className="w-[256px]">
-              <Text className="text-lg ml-4">{userEmail}</Text>
-              <Text className="ml-4 text-slate-500">Show Profile</Text>
-            </View>
-          </View>
-          <View className="mt-3">
-            <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
-          </View>
+    <View className="flex-row items-center pb-4 justify-between mt-9 border-b border-gray-300 h-20">
+      <View className="flex-row items-center">
+        <Image className="w-14 h-14 rounded-full" source={require(".././assets/images/profilePicture.png")} />
+        <View className="w-[256px]">
+          <Text className="text-lg ml-4">{userEmail}</Text>
+          <Text className="ml-4 text-slate-500">Show Profile</Text>
         </View>
-    </SafeAreaView>
+      </View>
+      <View className="mt-3">
+        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+      </View>
+    </View>
   );
 };
 
