6517e6717bbdbbb35e358cc86ad59e2000819465
Krishab Bashyal <krishab2002@gmail.com>
2024-05-20 18:15:31 -0500

    Formatted the settings list by using a component

--- components/ProfileSettingsList.jsx
+++ components/ProfileSettingsList.jsx
@@ -1,7 +1,8 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text } from "react-native";
 import React from "react";
 import { Feather } from "@expo/vector-icons";
 import SignOutButton from "./SignOutButton";
+import ProfileSettingsTab from "./ProfileSettingsTab"
 
 const ProfileSettingsList = () => {
   return (
@@ -9,27 +10,10 @@ const ProfileSettingsList = () => {
       <View className="mt-8 mb-2">
         <Text className="text-[22px] font-medium">Project Links</Text>
       </View>
-      {/* // View for the github icon */}
-      <View className="border-b py-2.5 border-gray-300">
-        <TouchableOpacity className="flex-row">
-          <Feather name="github" size={22} color="black" />
-          <Text className="ml-4 text-base">View Source Code</Text>
-        </TouchableOpacity>
-      </View>
-      {/* // View for the linkedin icon */}
-      <View className="border-b py-2.5 border-gray-300">
-        <TouchableOpacity className="flex-row">
-          <Feather name="linkedin" size={22} color="black" />
-          <Text className="ml-4 text-base">Developer LinkedIn</Text>
-        </TouchableOpacity>
-      </View>
-      {/* // View for something else */}
-      <View className="border-b py-2.5 border-gray-300">
-        <TouchableOpacity className="flex-row">
-          <Feather name="mail" size={22} color="black" />
-          <Text className="ml-4 text-base">Contant Me</Text>
-        </TouchableOpacity>
-      </View>
+
+      <ProfileSettingsTab iconName="github" text="View Source Code"/>
+      <ProfileSettingsTab iconName="linkedin" text="Developer LinkedIn"/>
+      <ProfileSettingsTab iconName="mail" text="Contact Me"/>
 
       <View className="mt-8">
         <Text className="text-[22px] font-medium">Profile and Settings</Text>
