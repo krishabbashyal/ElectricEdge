3d9b638fdb80a7982fe906a86aa247bd3787dc5d
Krishab Bashyal <krishab2002@gmail.com>
2024-06-08 15:06:38 -0500

    Started working on second page of the onboarding form (profile picture)

--- app/(auth)/profileSetup.jsx
+++ app/(auth)/profileSetup.jsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { SafeAreaView, View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 import { useState, useRef } from "react";
 import Checkbox from "expo-checkbox";
@@ -7,7 +7,6 @@ import CustomButton from "../../components/CustomButton";
 import CustomInputField from "../../components/CustomInputField";
 
 const profileSetup = () => {
-
   const [formData, setFormData] = useState({
     displayName: "",
     phoneNumber: "",
@@ -15,7 +14,8 @@ const profileSetup = () => {
   });
 
   const [formattedDisplayName, setFormattedDisplayName] = useState("");
-  const [checkboxError, setCheckboxError] = useState(false)
+  const [checkboxError, setCheckboxError] = useState(false);
+  const [formStep, setFormStep] = useState("");
 
   const displayNameRef = useRef(null);
   const phoneNumberRef = useRef(null);
@@ -34,77 +34,96 @@ const profileSetup = () => {
   };
 
   const handleAgreedToTermsChange = () => {
-    setFormData({ ...formData, agreedToTerms: !formData.agreedToTerms});
+    setFormData({ ...formData, agreedToTerms: !formData.agreedToTerms });
   };
 
   const getCheckboxValue = (checkboxValue) => {
-    setCheckboxError(!checkboxValue)
+    setCheckboxError(!checkboxValue);
     return checkboxValue;
-  }
+  };
 
   const submitForm = async () => {
+    setFormStep("profilePictureSetup");
     const displayNameValid = displayNameRef.current.validate();
     const phoneNumberValid = phoneNumberRef.current.validate();
-    const termsAgreed = getCheckboxValue(formData.agreedToTerms)
+    const termsAgreed = getCheckboxValue(formData.agreedToTerms);
 
-    if (termsAgreed){
-      setCheckboxError(false)
+    if (termsAgreed) {
+      setCheckboxError(false);
     }
 
     if (displayNameValid && phoneNumberValid && termsAgreed) {
       console.log(`Display Name: ${formData.displayName}`);
       console.log(`Phone Number: ${formData.phoneNumber}`);
       console.log(`Terms Agreed: ${formData.agreedToTerms}`);
+      setFormStep("profilePictureSetup");
     } else {
       console.log("Something does not look right, will not send to server");
       console.log(`Display Name: ${formData.displayName}`);
       console.log(`Phone Number: ${formData.phoneNumber}`);
       console.log(`Terms Agreed: ${formData.agreedToTerms}`);
-      console.log(displayNameValid, phoneNumberValid, termsAgreed)
+      console.log(displayNameValid, phoneNumberValid, termsAgreed);
     }
   };
 
-  return (
-    <SafeAreaView>
-      <ElectricEdgeHeader customStyles="mt-8" />
-      <View className="mx-8">
-        <Text className="font-bold text-3xl max-w-lg">Hello{formattedDisplayName}, It is nice to meet you. 👋 </Text>
-        <Text className="mb-4 text-gray-700 mt-2 text-[17px]">We just need a little more information before we can begin!</Text>
+  const renderPage = () => {
+    switch (formStep) {
+      case "profilePictureSetup":
+        return (
+          <View>
+            <ElectricEdgeHeader customStyles="mt-8" />
+            <View className="mx-8">
+              <Text className="font-bold text-3xl max-w-lg">Would you like to add a profile picture? </Text>
+              <Text className="mb-4 text-gray-700 mt-2 text-[17px]">Don't worry, you can always come back and do this later.</Text>
+              <Image className="w-72 h-72 rounded-full" source={require("../../assets/images/profilePicture.png")} />
+              <CustomButton title="Continue" buttonStyles="bg-EE-Green mt-8" textStyles="text-white" handlePress={submitForm} />
+              <CustomButton title="Go Back" buttonStyles="mt-8" textStyles="text-white" handlePress={submitForm} />
+            </View>
+          </View>
+        );
 
-        <CustomInputField
-          ref={displayNameRef}
-          label="What should we call you?"
-          placeholder="Display Name"
-          payload={formData.displayName}
-          validationType="String"
-          errorMessage="Display name must be at least 3 characters"
-          preventSpaces={true}
-          sendDataToParent={handleDisplayNameChange}
-          customStyles="mt-2"
-        />
+      default:
+        return (
+          <View>
+            <ElectricEdgeHeader customStyles="mt-8" />
+            <View className="mx-8">
+              <Text className="font-bold text-3xl max-w-lg">Hello{formattedDisplayName}, It is nice to meet you. 👋 </Text>
+              <Text className="mb-4 text-gray-700 mt-2 text-[17px]">We just need a little more information before we can begin!</Text>
+              <CustomInputField
+                ref={displayNameRef}
+                label="What should we call you?"
+                placeholder="Display Name"
+                payload={formData.displayName}
+                validationType="String"
+                errorMessage="Display name must be at least 3 characters"
+                preventSpaces={true}
+                sendDataToParent={handleDisplayNameChange}
+                customStyles="mt-2"
+              />
+              <CustomInputField
+                ref={phoneNumberRef}
+                label="What is a good phone number?"
+                placeholder="Phone Number"
+                payload={formData.phoneNumber}
+                validationType="PhoneNumber"
+                errorMessage="Please enter a valid phone number"
+                preventSpaces={true}
+                numericOnly={true}
+                sendDataToParent={handlePhoneNumberChange}
+                customStyles="pb-2"
+              />
+              <View className="flex flex-row items-center pt-4 border-t border-gray-300">
+                <Checkbox style={styles.checkbox} value={formData.agreedToTerms} onValueChange={handleAgreedToTermsChange} color={checkboxError ? "#BC6659" : "#3A8060"} />
+                <Text className={checkboxError ? "ml-2 text-EE-Red" : "ml-2"}>I understand that this application is just a prototype</Text>
+              </View>
+              <CustomButton title="Continue" buttonStyles="bg-EE-Green mt-8" textStyles="text-white" handlePress={submitForm} />
+            </View>
+          </View>
+        );
+    }
+  };
 
-        <CustomInputField
-          ref={phoneNumberRef}
-          label="What is a good phone number?"
-          placeholder="Phone Number"
-          payload={formData.phoneNumber}
-          validationType="PhoneNumber"
-          errorMessage="Please enter a valid phone number"
-          preventSpaces={true}
-          numericOnly={true}
-          sendDataToParent={handlePhoneNumberChange}
-          customStyles="pb-2"
-        />
-
-        <View className="flex flex-row items-center pt-4 border-t border-gray-300">
-          <Checkbox style={styles.checkbox} value={formData.agreedToTerms} onValueChange={handleAgreedToTermsChange} color={checkboxError ? "#BC6659" : "#3A8060"} />
-          <Text className={checkboxError ? "ml-2 text-EE-Red" : "ml-2"}>I understand that this application is just a prototype</Text>
-        </View>
-
-        <CustomButton title="Continue" buttonStyles="bg-EE-Green mt-8" textStyles="text-white" handlePress={submitForm} />
-      </View>
-    </SafeAreaView>
-  );
+  return <SafeAreaView>{renderPage()}</SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
