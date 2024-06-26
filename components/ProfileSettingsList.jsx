03676d8a1a7ebd77ca0340c16194a4ca80cd7ec0
Krishab Bashyal <krishab2002@gmail.com>
2024-06-25 20:24:09 -0500

    Made the links work on the profile page for developer information

--- components/ProfileSettingsList.jsx
+++ components/ProfileSettingsList.jsx
@@ -1,8 +1,8 @@
 import { View, Text } from "react-native";
 import React from "react";
 import SignOutButton from "./SignOutButton";
-import { Link } from "expo-router";
 import ProfileSettingsTab from "./ProfileSettingsTab";
+
 
 const ProfileSettingsList = () => {
   return (
@@ -10,9 +10,11 @@ const ProfileSettingsList = () => {
       <View className="mt-8 mb-2">
         <Text className="text-[22px] font-medium">Project Links</Text>
       </View>
-      <ProfileSettingsTab iconName="github" text="View Source Code" />
-      <ProfileSettingsTab iconName="linkedin" text="Developer LinkedIn" />
-      <ProfileSettingsTab iconName="mail" text="Contact Me" />
+      <View className="w-full">
+        <ProfileSettingsTab iconName="github" text="View Source Code" linkURL={"https://github.com/krishabbashyal"} />
+        <ProfileSettingsTab iconName="linkedin" text="Developer LinkedIn" linkURL={"https://www.linkedin.com/in/krishabbashyal"}/>
+        <ProfileSettingsTab iconName="mail" text="Contact Me" linkURL={"mailto:krishabbashyal@gmail.com"} />
+      </View>
       <View className="mt-8">
         <Text className="text-[22px] font-medium">Profile and Settings</Text>
         <SignOutButton />
--- components/ProfileSettingsTab.jsx
+++ components/ProfileSettingsTab.jsx
@@ -1,16 +1,17 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { Feather } from "@expo/vector-icons";
+import * as Linking from 'expo-linking'
 
 
-const ProfileSettingsTab = ({ iconName, text, linkTo }) => {
+const ProfileSettingsTab = ({ iconName, text, linkURL }) => {
   return (
-    <View className="border-b py-2.5 border-gray-300 w-full">
-      <TouchableOpacity className="flex-row w-full">
-        <Feather name={iconName} size={22} color="black" />
-        <Text className="ml-4 text-base">{text}</Text>
-      </TouchableOpacity>
-    </View>
+      <View className="border-b py-2.5 border-gray-300 w-full">
+        <TouchableOpacity className="flex-row w-full" onPress={() => Linking.openURL(linkURL)}>
+          <Feather name={iconName} size={22} color="black" />
+          <Text className="ml-4 text-base">{text}</Text>
+        </TouchableOpacity>
+      </View>
   );
 };
 
