d7241e9ef048ab8f79ab0872cb93a9a057f3e52c
Krishab Bashyal <krishab2002@gmail.com>
2024-06-27 17:57:38 -0500

    Got email on profile card by using the userContext

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useState, useContext } from "react";
+import { View, Text, Image } from "react-native";
 import { auth } from "../config/firebaseConfig";
 import { MaterialIcons } from "@expo/vector-icons";
+import { UserContext } from "../config/UserContext"
 
 const ProfileCard = () => {
-  const [userEmail, setUserEmail] = useState("");
-
-  useEffect(() => {
-    const user = auth.currentUser;
-    if (user) {
-      setUserEmail(user.email);
-    } else {
-      setUserEmail("No user logged in");
-    }
-  }, []);
+  const { currentUser } = useContext(UserContext)
 
   return (
     <View className="flex-row items-center pb-4 justify-between mt-9 border-b border-gray-300 h-20">
       <View className="flex-row items-center">
         <Image className="w-14 h-14 rounded-full" source={require(".././assets/images/profilePicture.png")} />
         <View className="w-[256px]">
-          <Text className="text-lg ml-4">{userEmail}</Text>
+          <Text className="text-lg ml-4">{currentUser ? currentUser.email : "No User"}</Text>
           <Text className="ml-4 text-slate-500">Show Profile</Text>
         </View>
       </View>
