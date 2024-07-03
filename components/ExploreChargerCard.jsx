918b1f2e38b69783c789130e70aad40693a24c7e
Krishab Bashyal <krishab2002@gmail.com>
2024-07-03 14:34:17 -0500

    Added touchable to charger cards

--- components/ExploreChargerCard.jsx
+++ components/ExploreChargerCard.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
 const ExploreChargerCard = ({ chargerType, chargerLocation, chargerRate, imageFile, cardStyles }) => {
@@ -6,15 +6,17 @@ const ExploreChargerCard = ({ chargerType, chargerLocation, chargerRate, imageFi
   return (
     <View className={`mb-8 ${cardStyles}`}>
       <View className="text-left">
-        <Image className="w-full h-[350px] rounded-xl" source={imageSource} />
-        <View className="pb-2">
-          <Text className="text-xl font-bold mt-3">{chargerType}</Text>
-          <Text className="text-lg ">{chargerLocation}</Text>
-          <Text className="text-lg font-bold ">
-            ${chargerRate}
-            <Text className="font-normal"> per hour</Text>
-          </Text>
-        </View>
+        <TouchableOpacity activeOpacity={0.7}>
+          <Image className="w-full h-[350px] rounded-xl" source={imageSource} />
+          <View className="pb-2">
+            <Text className="text-xl font-bold mt-3">{chargerType}</Text>
+            <Text className="text-lg ">{chargerLocation}</Text>
+            <Text className="text-lg font-bold ">
+              ${chargerRate}
+              <Text className="font-normal"> per hour</Text>
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
