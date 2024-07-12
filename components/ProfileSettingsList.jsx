0a28f0cf7fc63821254e8d13a4f98c1e67f511be
Krishab Bashyal <krishab2002@gmail.com>
2024-07-11 21:43:25 -0500

    Made changes to some of the casing

--- components/ProfileSettingsList.jsx
+++ components/ProfileSettingsList.jsx
@@ -8,15 +8,15 @@ const ProfileSettingsList = () => {
   return (
     <View>
       <View className="mt-8 mb-2">
-        <Text className="text-[22px] font-medium">Project Links</Text>
+        <Text className="text-[22px] font-medium">Project links</Text>
       </View>
       <View className="w-full">
-        <ProfileSettingsTab iconName="github" text="View Source Code" linkURL={"https://github.com/krishabbashyal"} />
-        <ProfileSettingsTab iconName="linkedin" text="Developer LinkedIn" linkURL={"https://www.linkedin.com/in/krishabbashyal"}/>
-        <ProfileSettingsTab iconName="mail" text="Contact Me" linkURL={"mailto:krishabbashyal@gmail.com"} />
+        <ProfileSettingsTab iconName="github" text="View source code" linkURL={"https://github.com/krishabbashyal"} />
+        <ProfileSettingsTab iconName="linkedin" text="Developer linkedIn" linkURL={"https://www.linkedin.com/in/krishabbashyal"}/>
+        <ProfileSettingsTab iconName="mail" text="Contact me" linkURL={"mailto:krishabbashyal@gmail.com"} />
       </View>
       <View className="mt-8">
-        <Text className="text-[22px] font-medium">Profile and Settings</Text>
+        <Text className="text-[22px] font-medium">Profile and settings</Text>
         <SignOutButton />
       </View>
     </View>
