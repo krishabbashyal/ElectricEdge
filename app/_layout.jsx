43e263c77e10323c725e9cf58b4eee27433a1ffa
Krishab Bashyal <krishab2002@gmail.com>
2024-07-12 18:34:44 -0500

    Added a new page for editing profiles

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -8,6 +8,7 @@ const RootLayout = () => {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="profile/editProfile" options={{ headerShown: true }} />
       </Stack>
     </UserProvider>
   );
