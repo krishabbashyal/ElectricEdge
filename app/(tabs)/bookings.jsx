cab41c92b142e37aa71ff26c56385a20f10d5bcf
Krishab Bashyal <krishab2002@gmail.com>
2024-07-11 21:31:33 -0500

    Created the empty pages for saved and bookings

--- app/(tabs)/bookings.jsx
+++ app/(tabs)/bookings.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
+import CustomButton from "../../components/CustomButton";
+import { router } from "expo-router";
 
 const Bookings = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <View className="mx-8">
-        <Text>Charged that you have booked will appear here</Text>
+        <View className="mt-16">
+          <Text className="text-3xl font-semibold">Confirmed bookings</Text>
+        </View>
+        <Text className="mt-4 text-xl font-semibold">No bookings have been made...yet!</Text>
+        <Text className="text-base mt-1">Don't get caught on the road with a dead car, find a charger for your next trip.</Text>
+        <CustomButton title="Start exploring" buttonStyles="w-40 h-12 bg-gray-900 mt-4" textStyles="text-white text-base" handlePress={() => router.push("/explore")} />
       </View>
     </SafeAreaView>
   );
--- app/(tabs)/saved.jsx
+++ app/(tabs)/saved.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
+import CustomButton from "../../components/CustomButton"
+import { router } from "expo-router";
 
 const Saved = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <View className="mx-8">
-        <Text>Charged that you have saved for later will appear here</Text>
+        <View className="mt-16">
+          <Text className="text-3xl font-semibold">Saved for later</Text>
+        </View>
+        <Text className="mt-4 text-xl font-semibold">Bummer, no chargers saved.</Text>
+        <Text className="text-base mt-1">Chargers that you've saved for later will appear here.</Text>
+        <CustomButton title="Start exploring" buttonStyles="w-40 h-12 bg-gray-900 mt-4" textStyles="text-white text-base" handlePress={() => router.push("/explore")} />
       </View>
     </SafeAreaView>
   );
