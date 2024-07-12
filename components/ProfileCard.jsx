37f2666bea41e5a5e43d647ed74745eb516e8d7e
Krishab Bashyal <krishab2002@gmail.com>
2024-07-11 20:33:25 -0500

    Made the profile card touchable

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { UserContext } from "../config/UserContext";
 import { db } from "../config/firebaseConfig";
@@ -24,24 +24,26 @@ const ProfileCard = () => {
   }, [])
 
   return (
-    <View className="flex-row items-center pb-4 justify-between mt-9 border-b border-gray-300 h-20">
-      <View className="flex-row items-center">
-        {currentUser && currentUser.photoURL ? (
-          <Image
-            className="w-16 h-16 rounded-full"
-            source={{ uri: currentUser.photoURL }}
-          />
-        ) : (
-          <Image className="w-16 h-16 rounded-full" source={require(".././assets/images/profilePicture.png")} />
-        )}
-        <View className="w-[256px]">
-          <Text className="text-lg ml-4">{displayName}</Text>
-          <Text className="ml-4 text-slate-500">Show Profile</Text>
+    <View className="border-b border-gray-300">
+      <TouchableOpacity className="flex-row items-center pb-4 justify-between mt-9 h-20">
+        <View className="flex-row items-center">
+          {currentUser && currentUser.photoURL ? (
+            <Image
+              className="w-16 h-16 rounded-full"
+              source={{ uri: currentUser.photoURL }}
+            />
+          ) : (
+            <Image className="w-16 h-16 rounded-full" source={require(".././assets/images/profilePicture.png")} />
+          )}
+          <View className="w-[256px]">
+            <Text className="text-lg ml-4">{displayName}</Text>
+            <Text className="ml-4 text-slate-500">Show Profile</Text>
+          </View>
         </View>
-      </View>
-      <View className="mt-3">
-        <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
-      </View>
+        <View className="mt-3">
+          <MaterialIcons name="arrow-forward-ios" size={18} color="black" />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
