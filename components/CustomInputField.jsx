753682322fada7ebbcf13064869d7c6dfb5ed069
Krishab Bashyal <krishab2002@gmail.com>
2024-07-12 19:12:12 -0500

    Updated CustomInputField to allow for default text

--- components/CustomInputField.jsx
+++ components/CustomInputField.jsx
@@ -1,12 +1,16 @@
 import { View, TextInput, Text, TouchableOpacity } from "react-native";
 import React, { forwardRef } from "react";
 import { Ionicons } from "@expo/vector-icons";
-import { useState, useImperativeHandle } from "react";
+import { useState, useImperativeHandle, useEffect } from "react";
 
-const CustomInputField = forwardRef(({ label, placeholder, errorMessage, keyboardType, customStyles, validationType, preventSpaces, numericOnly, sendDataToParent }, ref) => {
+const CustomInputField = forwardRef(({ label, placeholder, defaultValue, errorMessage, keyboardType, customStyles, validationType, preventSpaces, numericOnly, sendDataToParent }, ref) => {
   const [inputError, setInputError] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-  const [userInput, setUserInput] = useState("");
+  const [userInput, setUserInput] = useState(defaultValue || "");
+
+  useEffect(() => {
+    setUserInput(defaultValue || "");
+  }, [defaultValue]);
 
   useImperativeHandle(ref, () => ({
     validate() {
@@ -15,7 +19,6 @@ const CustomInputField = forwardRef(({ label, placeholder, errorMessage, keyboar
   }));
 
   const borderColor = inputError ? "border-EE-Red" : "border-EE-Green";
-
   const showPasswordIcon = showPassword ? <Ionicons name="eye-off-outline" size={24} color="black" /> : <Ionicons name="eye-outline" size={24} color="black" />;
 
   const formatPhoneNumber = (phoneNumber) => {
@@ -54,24 +57,24 @@ const CustomInputField = forwardRef(({ label, placeholder, errorMessage, keyboar
   const handleValidation = (userInput) => {
     let isValid = null;
 
-    if (validationType === "Email") {
-      isValid = emailValidation(userInput);
+    switch(validationType) {
+      case "Email":
+        isValid = emailValidation(userInput);
+        break;
+      case "Password":
+        isValid = passwordValidation(userInput);
+        break;
+      case "String":
+        isValid = stringValidation(userInput);
+        break;
+      case "PhoneNumber":
+        isValid = phoneNumberValidation(userInput);
+        break;
+      default:
+        isValid = true; // default to true if no validation type matches
     }
-    if (validationType === "Password") {
-      isValid = passwordValidation(userInput);
-    }
-    if (validationType === "String") {
-      isValid = stringValidation(userInput);
-    }
-    if (validationType === "PhoneNumber") {
-      isValid = phoneNumberValidation(userInput);
-    }
-    if (!isValid) {
-      setInputError(true);
-    }
-    if (isValid && inputError) {
-      setInputError(false);
-    }
+
+    setInputError(!isValid);
     return isValid;
   };
 
@@ -83,8 +86,8 @@ const CustomInputField = forwardRef(({ label, placeholder, errorMessage, keyboar
         placeholder={placeholder}
         secureTextEntry={label === "Password" && !showPassword}
         keyboardType={keyboardType}
-        onChangeText={(e) => {
-          let editedInput = e;
+        onChangeText={(text) => {
+          let editedInput = text;
 
           if (preventSpaces) {
             editedInput = editedInput.replace(/\s/g, "");
