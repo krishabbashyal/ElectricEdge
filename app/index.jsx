d4b66f0b85d38076ff8621f8fd41d32b8c8e2467
Krishab Bashyal <krishab2002@gmail.com>
2024-07-13 22:51:03 -0500

    Updated the button to check and see if there is a user already logged in when pressing get started

--- app/index.jsx
+++ app/index.jsx
@@ -9,9 +9,24 @@ import { UserContext } from "../config/UserContext"
 const ElectricEdge = () => {
   const { currentUser } = useContext(UserContext)
 
+
+  // useEffect(() => {
+  //   checkForUser()
+  // }, [])
+
+
+  // TODO: Replace these pushes to replaces once applicaiton is ready
   const checkForUser = () => {
     if (currentUser) {
-      router.replace('explore')
+      router.push('explore')
+    }
+  }
+
+  const handleButtonClick = () => {
+    if (currentUser) {
+      router.push('explore')
+    } else {
+      router.push('signUp')
     }
   }
 
@@ -23,7 +38,7 @@ const ElectricEdge = () => {
         <ElectricEdgeHeader />
         <Image className="w-full h-96" source={require("../assets/images/heroImage.png")} />
         <Text className="text-2xl text-center mx-6 font-medium">Charge Smarter, Drive Further, Enhance Your Journey.</Text>
-        <CustomButton title="Get Started" textStyles="text-white" buttonStyles="mt-6 mx-6 bg-EE-Green" handlePress={() => router.replace("/signUp")} />
+        <CustomButton title="Get Started" textStyles="text-white" buttonStyles="mt-6 mx-6 bg-EE-Green" handlePress={handleButtonClick} />
         <View className="flex flex-row justify-evenly mt-5 w-full">
           <CustomButton title="Explore Page" textStyles="text-xs font-medium" buttonStyles="h-7 w-32 bg-gray-300" handlePress={() => router.replace("/explore")} />
           <CustomButton title="Profile Picture Page" textStyles="text-xs font-medium" buttonStyles="h-7 w-32 bg-gray-300" handlePress={() => router.replace("/profilePictureSetup")} />
