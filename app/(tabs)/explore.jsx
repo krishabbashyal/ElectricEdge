909e2506b9d2a9526c9620d331bc55d482ee5894
Krishab Bashyal <krishab2002@gmail.com>
2024-07-16 14:58:45 -0500

    Added padding to last card on expore page

--- app/(tabs)/explore.jsx
+++ app/(tabs)/explore.jsx
@@ -17,7 +17,7 @@ const Explore = () => {
       querySnapshot.forEach((doc) => {
         chargers.push({ id: doc.id, ...doc.data() });
       });
-      console.log(chargers)
+      console.log(chargers);
       return chargers;
     } catch (error) {
       console.error("Error fetching documents: ", error);
@@ -37,20 +37,23 @@ const Explore = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <ExploreSearchAndFilter />
-      <QuickCityFilterScroller />
+      {/* <QuickCityFilterScroller /> */}
       <View className="mx-6 h-full mt-2">
         <ScrollView className="flex-1">
-          {chargers.map((charger) => (
-
-            <ExploreChargerCard
-              key={charger.id}
-              chargerType={charger.charger_type}
-              chargerCity={charger.city}
-              chargerState={charger.state}
-              chargerRate={charger.hourly_rate}
-              imageURL={charger.charger_image}
-            />
-          ))}
+          {chargers.map((charger, index) => {
+            const isLastItem = index === chargers.length - 1;
+            return (
+              <ExploreChargerCard
+                key={charger.id}
+                chargerType={charger.charger_type}
+                chargerCity={charger.city}
+                chargerState={charger.state}
+                chargerRate={charger.hourly_rate}
+                imageURL={charger.charger_image}
+                cardStyles={isLastItem ? "pb-16" : ""} // add padding to the last item
+              />
+            );
+          })}
         </ScrollView>
       </View>
     </SafeAreaView>
