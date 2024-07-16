07fdd3fb361e551c901735e26d28c86106aef878
Krishab Bashyal <krishab2002@gmail.com>
2024-07-15 19:44:02 -0500

    Updated component to accept database data

--- components/ExploreChargerCard.jsx
+++ components/ExploreChargerCard.jsx
@@ -1,16 +1,19 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 
-const ExploreChargerCard = ({ chargerType, chargerLocation, chargerRate, imageFile, cardStyles }) => {
-  const imageSource = imageFile;
+const ExploreChargerCard = ({ chargerType, chargerCity, chargerState, chargerRate, imageURL, cardStyles }) => {
   return (
     <View className={`mb-8 ${cardStyles}`}>
       <View className="text-left">
         <TouchableOpacity activeOpacity={0.7}>
-          <Image className="w-full h-[350px] rounded-xl" source={imageSource} />
+          <Image className="w-full h-[350px] rounded-xl" source={{uri: imageURL}} />
           <View className="pb-2">
             <Text className="text-xl font-bold mt-3">{chargerType}</Text>
-            <Text className="text-lg ">{chargerLocation}</Text>
+
+            <View className="flex flex-row">
+              <Text className="text-lg ">{chargerCity}, {chargerState}</Text>
+            </View>
+
             <Text className="text-lg font-bold ">
               ${chargerRate}
               <Text className="font-normal"> per hour</Text>
