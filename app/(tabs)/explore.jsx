7873b741d8964cd60e308ce761193a32dee0ca2b
Krishab Bashyal <krishab2002@gmail.com>
2024-07-15 19:43:26 -0500

    Got charger data from database

--- app/(tabs)/explore.jsx
+++ app/(tabs)/explore.jsx
@@ -1,47 +1,58 @@
+import React, { useState, useEffect } from "react";
 import { ScrollView, Text, View, SafeAreaView } from "react-native";
 import ExploreSearchAndFilter from "../../components/ExploreSearchAndFilter";
 import ExploreChargerCard from "../../components/ExploreChargerCard";
 import QuickCityFilterScroller from "../../components/QuickCityFilterScroller";
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebaseConfig";
+import { storage } from "../../config/firebaseConfig";
 
 const Explore = () => {
+  const [chargers, setChargers] = useState([]);
+
+  const fetchAllChargers = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "chargers"));
+      const chargers = [];
+      querySnapshot.forEach((doc) => {
+        chargers.push({ id: doc.id, ...doc.data() });
+      });
+      console.log(chargers)
+      return chargers;
+    } catch (error) {
+      console.error("Error fetching documents: ", error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    const getChargers = async () => {
+      const data = await fetchAllChargers();
+      setChargers(data);
+    };
+
+    getChargers();
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <ExploreSearchAndFilter />
+      <QuickCityFilterScroller />
+      <View className="mx-6 h-full mt-2">
+        <ScrollView className="flex-1">
+          {chargers.map((charger) => (
 
-        <QuickCityFilterScroller />
-        <View className="mx-6 h-full mt-2">
-          <ScrollView className="flex-1">
-            <View className="flex-col">
-              <ExploreChargerCard
-                imageFile={require("../../assets/images/dallas.jpeg")}
-                chargerType="Level 2: 240-Volt Outlet"
-                chargerLocation="Dallas, TX"
-                chargerRate="2.37"
-              />
-              <ExploreChargerCard
-                imageFile={require("../../assets/images/boston.jpeg")}
-                chargerType="Level 2: 240-Volt Outlet"
-                chargerLocation="Boston, MA"
-                chargerRate="2.37"
-              />
-              <ExploreChargerCard
-                imageFile={require("../../assets/images/losAngeles.jpeg")}
-                chargerType="Level 2: 240-Volt Outlet"
-                chargerLocation="Los Angeles, CA"
-                chargerRate="2.37"
-              />
-              <ExploreChargerCard
-                imageFile={require("../../assets/images/newYork.jpeg")}
-                chargerType="Level 2: 240-Volt Outlet"
-                chargerLocation="New York City, NY"
-                chargerRate="2.37"
-                cardStyles={"mb-48"}
-              />
-            </View>
-          </ScrollView>
-        </View>
-
+            <ExploreChargerCard
+              key={charger.id}
+              chargerType={charger.charger_type}
+              chargerCity={charger.city}
+              chargerState={charger.state}
+              chargerRate={charger.hourly_rate}
+              imageURL={charger.charger_image}
+            />
+          ))}
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
