510bc014da5998157b0e2cf162e55dd7ec143f37
Krishab Bashyal <krishab2002@gmail.com>
2024-07-16 17:44:00 -0500

    Started breaking apart code block into components

--- app/details/[id].jsx
+++ app/details/[id].jsx
@@ -4,6 +4,9 @@ import { db } from "../../config/firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
 import { useLocalSearchParams } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import ChargerHostInformation from "../../components/ChargerHostInformation";
+import ChargerListingDescription from "../../components/ChargerListingDescription";
+import ChargerListingOverview from "../../components/ChargerListingOverview";
 
 const Details = () => {
   const [chargerData, setChargerData] = useState("");
@@ -29,21 +32,9 @@ const Details = () => {
       <StatusBar style="light" />
       <Image className="w-full h-[400px]" source={{ uri: chargerData.charger_image }} />
       <View className="mx-6">
-        <View>
-          <Text className="text-2xl font-bold mt-4">{chargerData.charger_type}</Text>
-          <View className="flex flex-row">
-            <Text className="text-lg font-medium">
-              Charging station in <Text className='font-bold text-EE-Green'>{chargerData.city}, {chargerData.state}</Text>
-            </Text>
-          </View>
-          <View className="mt-4 flex flex-row">
-            <Image className="h-14 border w-14 rounded-full" source={{ uri: chargerData.host_image }}/>
-            <Text>Hosted by {chargerData.host_display_name}</Text>
-          </View>
-          <View className="mt-4 border-t  border-gray-300 pt-4">
-            <Text className="text-base">{chargerData.description}</Text>
-          </View>
-        </View>
+        <ChargerListingOverview chargerType={chargerData.charger_type} city={chargerData.city} state={chargerData.state} />
+        <ChargerHostInformation hostDisplayName={chargerData.host_display_name} hostProfilePicture={chargerData.host_image} />
+        <ChargerListingDescription description={chargerData.description} />
       </View>
     </>
   );
