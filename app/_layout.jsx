5dfcc91b2381f85efe1567b37ec4a1ad15a5383e
Krishab Bashyal <krishab2002@gmail.com>
2024-07-16 17:16:42 -0500

    Added a new route for chargerDetails

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import UserProvider from "../config/UserContext";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
   return (
@@ -10,6 +11,8 @@ const RootLayout = () => {
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="profile/editProfile" options={{ headerShown: false }} />
         <Stack.Screen name="profile/chargerListing" options={{ headerShown: false }} />
+        <Stack.Screen name="details/[id]" options={{ headerShown: false}} />
+
       </Stack>
     </UserProvider>
   );
