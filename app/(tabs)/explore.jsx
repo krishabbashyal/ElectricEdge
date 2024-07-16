26eea178d8984d7d3c1b5fc31e57ca7fdf7e96f2
Krishab Bashyal <krishab2002@gmail.com>
2024-07-16 17:17:09 -0500

    Passed the data when a charger is pressed on the explore page

--- app/(tabs)/explore.jsx
+++ app/(tabs)/explore.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, Text, View, SafeAreaView } from "react-native";
+import { ScrollView, Text, View, SafeAreaView, TouchableOpacity } from "react-native";
 import ExploreSearchAndFilter from "../../components/ExploreSearchAndFilter";
 import ExploreChargerCard from "../../components/ExploreChargerCard";
 import QuickCityFilterScroller from "../../components/QuickCityFilterScroller";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../config/firebaseConfig";
 import { storage } from "../../config/firebaseConfig";
+import { router } from "expo-router";
 
 const Explore = () => {
   const [chargers, setChargers] = useState([]);
@@ -17,7 +18,6 @@ const Explore = () => {
       querySnapshot.forEach((doc) => {
         chargers.push({ id: doc.id, ...doc.data() });
       });
-      console.log(chargers);
       return chargers;
     } catch (error) {
       console.error("Error fetching documents: ", error);
@@ -34,6 +34,16 @@ const Explore = () => {
     getChargers();
   }, []);
 
+  const handleChargerClick = (chargerID) => {
+    console.log(`Charger ${chargerID} pressed`)
+    router.push({
+      pathname: `details/${chargerID}`,
+      params: {
+        chargerID: chargerID
+      }
+    })
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <ExploreSearchAndFilter />
@@ -43,15 +53,16 @@ const Explore = () => {
           {chargers.map((charger, index) => {
             const isLastItem = index === chargers.length - 1;
             return (
-              <ExploreChargerCard
-                key={charger.id}
-                chargerType={charger.charger_type}
-                chargerCity={charger.city}
-                chargerState={charger.state}
-                chargerRate={charger.hourly_rate}
-                imageURL={charger.charger_image}
-                cardStyles={isLastItem ? "pb-16" : ""} // add padding to the last item
-              />
+              <TouchableOpacity key={charger.id} activeOpacity={0.7} onPress={() => (handleChargerClick(charger.id))}>
+                <ExploreChargerCard
+                  chargerType={charger.charger_type}
+                  chargerCity={charger.city}
+                  chargerState={charger.state}
+                  chargerRate={charger.hourly_rate}
+                  imageURL={charger.charger_image}
+                  cardStyles={isLastItem ? "pb-16" : ""} // add padding to the last item
+                />
+              </TouchableOpacity>
             );
           })}
         </ScrollView>
