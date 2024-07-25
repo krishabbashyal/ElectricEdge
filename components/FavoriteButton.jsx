6e68ae2043e99b2e5eb84f3e7f93fba6b1085206
Krishab Bashyal <krishab2002@gmail.com>
2024-07-24 21:48:22 -0500

    Started working on the fav button

--- components/FavoriteButton.jsx
+++ components/FavoriteButton.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, TouchableOpacity } from "react-native";
+import React from "react";
+import { AntDesign } from '@expo/vector-icons';
 
-const FavoriteButton = () => {
+const FavoriteButton = ({ customStyles }) => {
+  const handleFavoritePressed = () => {};
+
+  const handleFavoriteUnpressed = () => {};
+
   return (
-    <View>
-      <Text>FavoriteButton</Text>
-    </View>
-  )
-}
+    <TouchableOpacity className={`z-10 ${customStyles} absolute`} activeOpacity={1}>
+      <View className="p-1.5 bg-white rounded-full">
+      <AntDesign name="heart" size={20} color="black" />
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default FavoriteButton
+export default FavoriteButton;
