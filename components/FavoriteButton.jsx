d4b54fcd055ba4e229dec6dd14e94697139d947f
Krishab Bashyal <krishab2002@gmail.com>
2024-07-24 21:34:37 -0500

    Boiller plate for favorite buttons

---
+++ components/FavoriteButton.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FavoriteButton = () => {
+  return (
+    <View>
+      <Text>FavoriteButton</Text>
+    </View>
+  )
+}
+
+export default FavoriteButton
