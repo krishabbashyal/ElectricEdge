a573a34e5fc7f32403bff10a090f9573e344e378
Krishab Bashyal <krishab2002@gmail.com>
2024-07-26 01:42:17 -0500

    Cleaned up explore page

--- components/ExploreChargerCard.jsx
+++ components/ExploreChargerCard.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image} from "react-native";
 import React from "react";
 
 const ExploreChargerCard = ({ chargerType, chargerCity, chargerState, chargerRate, imageURL, cardStyles }) => {
