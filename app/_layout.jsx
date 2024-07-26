85ece4451862d5b5bb2edacaf86fd8fe655e7887
Krishab Bashyal <krishab2002@gmail.com>
2024-07-25 22:16:54 -0500

    Added a wrapper to use the new context in the application

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,19 +1,21 @@
 import { Stack } from "expo-router";
 import UserProvider from "../config/UserContext";
+import UserProfileProvider from "../config/UserProfileContext";
 import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
   return (
     <UserProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="profile/editProfile" options={{ headerShown: false }} />
-        <Stack.Screen name="profile/chargerListing" options={{ headerShown: false }} />
-        <Stack.Screen name="details/[id]" options={{ headerShown: false}} />
-
-      </Stack>
+      <UserProfileProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="profile/editProfile" options={{ headerShown: false }} />
+          <Stack.Screen name="profile/chargerListing" options={{ headerShown: false }} />
+          <Stack.Screen name="details/[id]" options={{ headerShown: false}} />
+        </Stack>
+      </UserProfileProvider>
     </UserProvider>
   );
 };
