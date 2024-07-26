36d90bff9aff442b91d438d6a72344ea69366b27
Krishab Bashyal <krishab2002@gmail.com>
2024-07-26 01:41:26 -0500

    Worked on favorite button

--- components/FavoriteButton.jsx
+++ components/FavoriteButton.jsx
@@ -2,8 +2,12 @@ import { View, TouchableOpacity } from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { UserProfileContext } from "../config/UserProfileContext";
+import { doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore";
+import { db } from "../config/firebaseConfig";
+import { UserContext } from "../config/UserContext";
 
 const FavoriteButton = ({ customStyles, chargerID }) => {
+  const { currentUser } = useContext(UserContext)
   const { userData } = useContext(UserProfileContext);
   const [chargerSaved, setChargerSaved] = useState();
 
@@ -12,9 +16,27 @@ const FavoriteButton = ({ customStyles, chargerID }) => {
     setChargerSaved(isSaved);
   }, [userData, chargerID]);
 
-  const toggleFavorite = () => {
-    setChargerSaved(prevState => !prevState);
-    // Add backend update logic here if needed
+  const toggleFavorite = async () => {
+    setChargerSaved((prevState) => !prevState);
+
+    const profileRef = doc(db, "profiles", currentUser.uid);
+
+    try {
+      if (chargerSaved) {
+        // If charger is currently saved, remove it
+        await updateDoc(profileRef, {
+          saved_chargers: arrayRemove(chargerID),
+        });
+      } else {
+        // If charger is not saved, add it
+        await updateDoc(profileRef, {
+          saved_chargers: arrayUnion(chargerID),
+        });
+      }
+      setChargerSaved(!chargerSaved); // Update local state
+    } catch (error) {
+      console.error("Failed to update favorite status", error);
+    }
   };
 
   return (
