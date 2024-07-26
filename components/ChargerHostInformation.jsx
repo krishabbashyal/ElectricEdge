9021a9e25c047f5addc96cbad733d5ba2ae92c06
Krishab Bashyal <krishab2002@gmail.com>
2024-07-26 02:37:05 -0500

    hardcoded data since i deleted on accident

--- components/ChargerHostInformation.jsx
+++ components/ChargerHostInformation.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ChargerHostInformation = ({hostDisplayName, hostProfilePicture}) => {
   return (
     <View className="border-t border-gray-300 flex flex-row items-center mt-4 pt-3">
-    <Image className="h-12 border w-12 rounded-full" source={{ uri: hostProfilePicture }}/>
+    <Image className="h-12 border w-12 rounded-full" source={require("../assets/images/OpenAILogo.jpg")}/>
     <View>
       <Text className="ml-4 font-medium text-base">Hosted by {hostDisplayName}</Text>
       <Text className="ml-4 -mt-0.5 text-sm text-gray-500">AI generated charger listing</Text>
