b38b7711aaf368c4d69269bfaed7565142dfa089
Krishab Bashyal <krishab2002@gmail.com>
2024-07-26 01:40:29 -0500

    added provider to layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,47 +1,50 @@
 import React from "react";
 import { Tabs } from "expo-router";
 import UserProvider from "../../config/UserContext";
+import UserProfileProvider from "../../config/UserProfileContext";
 import { Feather } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 
 const TabsLayout = () => {
   return (
     <UserProvider>
+      <UserProfileProvider>
       <StatusBar style="dark" />
-      <Tabs screenOptions={{ tabBarActiveTintColor: "#3A8060", tabBarInactiveTintColor: "#A2AFA9", tabBarStyle: { backgroundColor: "#fff", height: 90 } }}>
-        <Tabs.Screen
-          name="explore"
-          options={{
-            title: "Explore",
-            tabBarIcon: ({ color }) => <Feather name="battery-charging" size={32} color={color} />,
-            headerShown: false,
-          }}
-        />
-        <Tabs.Screen
-          name="saved"
-          options={{
-            title: "Saved",
-            tabBarIcon: ({ color }) => <Feather name="heart" size={28} color={color} />,
-            headerShown: false,
-          }}
-        />
-        <Tabs.Screen
-          name="bookings"
-          options={{
-            title: "Bookings",
-            tabBarIcon: ({ color }) => <Feather name="check-circle" size={28} color={color}/>,
-            headerShown: false,
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            title: "Profile",
-            tabBarIcon: ({ color }) => <Feather name="user" size={28} color={color} />,
-            headerShown: false,
-          }}
-        />
-      </Tabs>
+        <Tabs screenOptions={{ tabBarActiveTintColor: "#3A8060", tabBarInactiveTintColor: "#A2AFA9", tabBarStyle: { backgroundColor: "#fff", height: 90 } }}>
+          <Tabs.Screen
+            name="explore"
+            options={{
+              title: "Explore",
+              tabBarIcon: ({ color }) => <Feather name="battery-charging" size={32} color={color} />,
+              headerShown: false,
+            }}
+          />
+          <Tabs.Screen
+            name="saved"
+            options={{
+              title: "Saved",
+              tabBarIcon: ({ color }) => <Feather name="heart" size={28} color={color} />,
+              headerShown: false,
+            }}
+          />
+          <Tabs.Screen
+            name="bookings"
+            options={{
+              title: "Bookings",
+              tabBarIcon: ({ color }) => <Feather name="check-circle" size={28} color={color}/>,
+              headerShown: false,
+            }}
+          />
+          <Tabs.Screen
+            name="profile"
+            options={{
+              title: "Profile",
+              tabBarIcon: ({ color }) => <Feather name="user" size={28} color={color} />,
+              headerShown: false,
+            }}
+          />
+        </Tabs>
+      </UserProfileProvider>
     </UserProvider>
   );
 };
