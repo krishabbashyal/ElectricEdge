d96b5f8666c39e79e8210a8dc20f2f99c06a61fd
Krishab Bashyal <krishab2002@gmail.com>
2024-07-25 23:06:47 -0500

    Got the favorite button toggle working

--- components/FavoriteButton.jsx
+++ components/FavoriteButton.jsx
@@ -1,20 +1,26 @@
 import { View, TouchableOpacity } from "react-native";
-import React, { useEffect } from "react";
-import { AntDesign } from '@expo/vector-icons';
+import React, { useContext, useEffect, useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import { UserProfileContext } from "../config/UserProfileContext";
 
 const FavoriteButton = ({ customStyles, chargerID }) => {
-  const handleFavoritePressed = () => {};
-
-  const handleFavoriteUnpressed = () => {};
+  const { userData } = useContext(UserProfileContext);
+  const [chargerSaved, setChargerSaved] = useState();
 
   useEffect(() => {
-    console.log(chargerID)
-  }, [])
+    const isSaved = userData?.saved_chargers?.includes(chargerID) || false;
+    setChargerSaved(isSaved);
+  }, [userData, chargerID]);
+
+  const toggleFavorite = () => {
+    setChargerSaved(prevState => !prevState);
+    // Add backend update logic here if needed
+  };
 
   return (
-    <TouchableOpacity className={`z-10 ${customStyles} absolute`} activeOpacity={1}>
+    <TouchableOpacity className={`z-10 ${customStyles} absolute`} activeOpacity={1} onPress={toggleFavorite}>
       <View className="p-1.5 bg-white rounded-full">
-      <AntDesign name="hearto" size={20} color="black" />
+        <AntDesign name={chargerSaved ? "heart" : "hearto"} size={21} color={chargerSaved ? "#FF4263" : "black"} />
       </View>
     </TouchableOpacity>
   );
