c5e53ba1e63f2716bcfe43d7dff40b3c3608dec9
Krishab Bashyal <krishab2002@gmail.com>
2024-07-26 02:37:25 -0500

    Moved around content in compoenet to render when content is present

--- components/EmptySection.jsx
+++ components/EmptySection.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { View, Text } from "react-native";
-const EmptySection = ({ sectionTitle, subHeader, descriptionText }) => {
+const EmptySection = ({ subHeader, descriptionText }) => {
   return (
     <View>
-      <View className="mt-16">
-        <Text className="text-3xl font-semibold">{sectionTitle}</Text>
-      </View>
       <Text className="mt-4 text-xl font-semibold">{subHeader}</Text>
       <Text className="text-base mt-1">{descriptionText}</Text>
     </View>
