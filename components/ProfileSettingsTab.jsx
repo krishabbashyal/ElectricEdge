6a0816fe3180f9efc462002c68539d4f651f19fc
Krishab Bashyal <krishab2002@gmail.com>
2024-08-22 13:53:45 -0500

    Added a size prop for the icons in profile settings tab

--- components/ProfileSettingsTab.jsx
+++ components/ProfileSettingsTab.jsx
@@ -4,11 +4,11 @@ import { Feather } from "@expo/vector-icons";
 import * as Linking from 'expo-linking'
 
 
-const ProfileSettingsTab = ({ iconName, text, linkURL }) => {
+const ProfileSettingsTab = ({ iconName, text, linkURL, size }) => {
   return (
       <View className="border-b py-2.5 border-gray-300 w-full">
         <TouchableOpacity className="flex-row w-full" onPress={() => Linking.openURL(linkURL)}>
-          <Feather name={iconName} size={22} color="black" />
+          <Feather name={iconName} size={size || 22 } color="black" />
           <Text className="ml-4 text-base">{text}</Text>
         </TouchableOpacity>
       </View>
