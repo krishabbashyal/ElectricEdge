6fe892f6ed1f966c1820bc50b4f14c32e7b0d0fb
Krishab Bashyal <krishab2002@gmail.com>
2024-08-22 20:46:38 -0500

    Added show error prop and some code formatting

--- components/PaymentMethods.jsx
+++ components/PaymentMethods.jsx
@@ -3,24 +3,26 @@ import React, { useEffect, useState } from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
-const PaymentMethods = ({onSendData}) => {
+const PaymentMethods = ({ onSendData, showError }) => {
   const [activeMethod, setActiveMethod] = useState();
 
   useEffect(() => {
     if (activeMethod !== undefined) {
-      onSendData(activeMethod)
+      onSendData(activeMethod);
     }
   }, [activeMethod, onSendData]);
 
   return (
     <View className="mt-2">
-      <TouchableOpacity activeOpacity={1.0}
+      <TouchableOpacity
+        activeOpacity={1.0}
         className={`border h-15 p-4 rounded-xl border-gray-400 border- flex flex-row items-center mb-2 ${activeMethod === "PayPal" ? "border-gray-800 border-[1.5px]" : ""}`}
         onPress={() => setActiveMethod("PayPal")}>
         <FontAwesome name="paypal" size={24} color="black" />
         <Text className="ml-6 text-base">PayPal</Text>
       </TouchableOpacity>
-      <TouchableOpacity activeOpacity={1.0}
+      <TouchableOpacity
+        activeOpacity={1.0}
         className={`border h-15 p-4 rounded-xl border-gray-400 flex flex-row items-center mb-2 ${activeMethod === "Credit Card" ? "border-gray-800 border-[1.5px]" : ""}`}
         onPress={() => setActiveMethod("Credit Card")}>
         <FontAwesome name="cc-discover" size={24} color="black" />
@@ -29,12 +31,18 @@ const PaymentMethods = ({onSendData}) => {
           <Text className="ml-1 text-base">8542</Text>
         </View>
       </TouchableOpacity>
-      <TouchableOpacity activeOpacity={1.0}
+      <TouchableOpacity
+        activeOpacity={1.0}
         className={`border h-15 p-4 rounded-xl border-gray-400 flex flex-row items-center mb-2 ${activeMethod === "Apple Pay" ? "border-gray-800 border-[1.5px]" : ""}`}
         onPress={() => setActiveMethod("Apple Pay")}>
         <FontAwesome5 name="apple-pay" size={26} color="black" />
         <Text className="ml-4 text-base">Apple Pay</Text>
       </TouchableOpacity>
+      {showError ? (
+        <View className="mt-2 h-10 bg-red-200 rounded-full">
+          <Text className="text-sm text-red-500 text-center mt-2.5 font-semibold">Please select a payment method</Text>
+        </View>
+      ) : null}
     </View>
   );
 };
